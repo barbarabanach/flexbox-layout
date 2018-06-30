049d32b62975ec688ffb7ea5e782f45ffbf20392
Basia <krzewicka.barbara@gmail.com>
2018-06-30 20:31:44 +0200

    flexbox layout first part

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            files: 'app/**/*.scss',
+            tasks: ['sass']
+        },
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'app/css/style.css': 'app/scss/style.scss'
+                }
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'app/css/*.css',
+                        'app/*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './app'
+                }
+            }
+        }
+    });
+
+    // load npm tasks
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    // define default task
+    grunt.registerTask('default', ['browserSync', 'watch', 'sass']);
+};
